2d01e241d8dcdcb00f1d0f83dad71c6e836fd8f4
marb61a <marb61a@gmail.com>
2019-01-17 21:08:55 +0000

    updated tooltip

--- tooltip.js
+++ tooltip.js
@@ -1,5 +1,17 @@
 class Tooltip extends HTMLElement {
   constructor() {
-    
+    super();
+  }
+
+  connectedCallback() {
+    const tooltipIcon = document.createElement('span');
+    tooltipIcon.textContent = ' ?';
+    this.appendChild(tooltipIcon);
+
   }
 }
+
+// The name of the element has to have a dash
+// Names should be as unique as possible in 
+// order ro avoid clashing with other users custom elements
+customElements.define('marb-tooltip', Tooltip);
